1f32cd1d1945efe036eab5b5d59355c084f85cd6
Tanaka I. Kahwai <iankahwai@gmail.com>
2023-02-11 10:33:28 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Tanaka on 11/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
